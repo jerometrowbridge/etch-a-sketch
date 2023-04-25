dc48a6766b90e880bf3e64249ae4d275fb9bec21
Jerome Trowbridge <122302999+jerometrowbridge@users.noreply.github.com>
2023-04-25 10:46:04 -0500

    Add grid functionality based on user input

---
+++ etch.js
@@ -0,0 +1,21 @@
+
+const gridspace = document.getElementById("gridspace")
+function createGrid() {
+    gridspace.innerHTML = ""
+    let userRows = document.getElementById("myRows").value;
+    let userColumns = document.getElementById("myColumns").value;
+    gridspace.style.gridTemplateColumns = `repeat(${userColumns}, 20px)`;
+    gridspace.style.gridTemplateRows = `repeat(${userRows}, 20px)`;
+    for (let i = 0; i < userRows * userColumns; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        gridspace.appendChild(cell);
+    }
+}
+
+
+
+
+const gridButton = document.getElementById("gridButton");
+gridButton.addEventListener('click', createGrid)
+
